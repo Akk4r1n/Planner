c36fac3a2b8c6998688643a3a5ac6c1d013dcc6d
akk4r1n <davidriewe01@gmail.com>
2021-04-14 22:43:03 +0200

    Setup a basic kanban board

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
+  backlog = [
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep'
+  ];
+
+  todo = [
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
+
+  doing = [
+    'Task 1',
+    'Task 2',
+    'Task 3'
+  ];
+
+  done = [
+    'Task 4',
+    'Task 5',
+    'Task 6'
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
   }
 
 }
